c82f64ae20ad173039f7fc6b42a56215a095cedd
Daniel Braun <daniel.braun@outlook.co.il>
2022-11-30 17:02:34 +0000

    more verbosity

--- test/haskell/test.sh
+++ test/haskell/test.sh
@@ -5,10 +5,17 @@ set -x
 source dev-container-features-test-lib
 
 echo $USERNAME
+echo $_REMOTE_USER
 echo $USER
 whoami 
 id
 
+sudo -iu "$USERNAME" <<EOF
+    echo $USERNAME
+    echo $USER
+    whoami 
+    id
+EOF
 
 # we are simply checking existance for now.
 check "ghci existance" ghci --version
