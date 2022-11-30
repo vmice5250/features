711742ffd3f06d3bc182a7f522163367f74cedc0
Daniel Braun <daniel.braun@outlook.co.il>
2022-11-30 17:16:45 +0000

    attempt to run as non root user

--- test/haskell/test.sh
+++ test/haskell/test.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 source dev-container-features-test-lib
 
-echo $USERNAME
-echo $_REMOTE_USER
-echo $USER
-whoami 
-id
+
+USERNAME=""
+POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+    if id -u ${CURRENT_USER} >/dev/null 2>&1; then
+        USERNAME=${CURRENT_USER}
+        break
+    fi
+done
+if [ "${USERNAME}" = "" ]; then
+    echo "could not resolve a non root user"
+    exit 1
+fi
+
+
+echo "running test with user: $USERNAME"
+
 
 sudo -iu "$USERNAME" <<EOF
-    echo $USERNAME
-    echo $USER
-    whoami 
-    id
+    check "ghci existance" ghci --version
+    check "cabal existance" cabal --version
 EOF
-
-# we are simply checking existance for now.
-check "ghci existance" ghci --version
-check "cabal existance" cabal --version
-
-
 
 reportResults
