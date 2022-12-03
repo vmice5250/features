c16ff89f4386240e0c873363a982648e5f45fbed
Daniel Braun <daniel.braun@outlook.co.il>
2022-12-03 16:09:42 +0000

    source in the right place

--- test/digitalocean-cli/test.sh
+++ test/digitalocean-cli/test.sh
@@ -2,26 +2,26 @@
 
 set -e
 
-source dev-container-features-test-lib
 
 
-USERNAME="${USERNAME:-"${_REMOTE_USER:-""}"}"
-if [ "${USERNAME}" = "" ]; then
-    USERNAME=""
-    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
-            USERNAME=${CURRENT_USER}
-            break
-        fi
-    done
-    if [ "${USERNAME}" = "" ]; then
-        USERNAME=root
+
+USERNAME="${_REMOTE_USER:-""}"
+POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+    if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+        USERNAME=${CURRENT_USER}
+        break
     fi
+done
+if [ "${USERNAME}" = "" ]; then
+    USERNAME=root
 fi
 
 
+
 sudo su - "$USERNAME" <<EOF
+    source dev-container-features-test-lib
+
     check "doctl cli existance" doctl version
 EOF
 
