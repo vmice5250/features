4a282d1942e7462f9fa7d03eda4182fd17b8065c
Daniel Braun <daniel.braun@outlook.co.il>
2022-12-03 15:56:15 +0000

    trying new username resolve mechanism

--- test/digitalocean-cli/test.sh
+++ test/digitalocean-cli/test.sh
@@ -4,7 +4,24 @@ set -e
 
 source dev-container-features-test-lib
 
-su - "$_REMOTE_USER" <<EOF
+
+USERNAME="${USERNAME:-"${_REMOTE_USER:-""}"}"
+if [ "${USERNAME}" = "" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+fi
+
+
+su - "$USERNAME" <<EOF
     check "doctl cli existance" doctl version
 EOF
 
