7d2ac5cfe5f266c19cbe991f68a0af00c0114901
Daniel Braun <braundaniel@live.com>
2023-04-01 23:54:40 +0000

    enable  force on non -ubuntu

--- src/neovim-apt-get/install.sh
+++ src/neovim-apt-get/install.sh
@@ -16,7 +16,7 @@ $nanolayer_location \
     install \
     devcontainer-feature \
     "ghcr.io/devcontainers-contrib/features/apt-get-packages:1.0.2" \
-    --option packages="neovim" --option ppas="ppa:neovim-ppa/stable"
+    --option packages="neovim" --option ppas="ppa:neovim-ppa/stable" --option force_ppas_on_non_ubuntu="True"
 
 
 echo 'Done!'
