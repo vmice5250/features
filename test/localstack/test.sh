bdd29f02be1471cd10b88c0b808cf18e6be9f715
Daniel Braun <braundaniel@live.com>
2023-05-01 11:47:39 +0000

    feat: localstack now installs dind and python 3.10

--- test/localstack/test.sh
+++ test/localstack/test.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "localstack --version" localstack --version
+check "localstack --version && localstack start -d && sleep 10 && localstack status docker && localstack status services && localstack stop" localstack --version && localstack start -d && sleep 10 && localstack status docker && localstack status services && localstack stop
 
 reportResults
