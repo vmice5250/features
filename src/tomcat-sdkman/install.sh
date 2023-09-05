cbdf04740ef18beaae32667bd3df5a65e4f5e516
Daniel Braun <braundaniel@live.com>
2023-09-05 14:55:54 +0000

    feat: update gh-release dependency to 1-0-20 version

--- src/tomcat-sdkman/install.sh
+++ src/tomcat-sdkman/install.sh
@@ -1,15 +1,14 @@
-#!/bin/bash -i
 
 set -e
 
-source ./library_scripts.sh
+. ./library_scripts.sh
 
 # nanolayer is a cli utility which keeps container layers as small as possible
 # source code: https://github.com/devcontainers-contrib/nanolayer
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.4.29"
+ensure_nanolayer nanolayer_location "v0.5.2"
 
 
 $nanolayer_location \
@@ -17,6 +16,7 @@ $nanolayer_location \
     devcontainer-feature \
     "ghcr.io/ebaskoro/devcontainer-features/sdkman:1.0.0" \
     --option candidate='none' --option version='none'
+    
 
 
 $nanolayer_location \
@@ -24,6 +24,7 @@ $nanolayer_location \
     devcontainer-feature \
     "ghcr.io/devcontainers-contrib/features/bash-command:1.0.0" \
     --option command='echo sdkman_auto_answer=true > ${SDKMAN_DIR}/etc/config'
+    
 
 
 $nanolayer_location \
@@ -31,6 +32,7 @@ $nanolayer_location \
     devcontainer-feature \
     "ghcr.io/devcontainers/features/java:1.2.1" \
     --option jdkDistro="$JDKDISTRO" --option version="$JDKVERSION"
+    
 
 
 $nanolayer_location \
@@ -38,6 +40,7 @@ $nanolayer_location \
     devcontainer-feature \
     "ghcr.io/ebaskoro/devcontainer-features/sdkman:1.0.0" \
     --option candidate='tomcat' --option version="$VERSION"
+    
 
 
 echo 'Done!'
